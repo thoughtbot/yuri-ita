eeb558bc26be292a1ffd730231255e2aef81ef9d
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2020-01-24 17:35:43 -0500

    bundle gem yuri-ita
    Initial gem creation with no changes.
    
    The name "Yuri-ita" comes from the Japanese name for a gold pan. We're
    filtering and searching for gold!

---
+++ lib/yuri/ita.rb
@@ -0,0 +1,8 @@
+require "yuri/ita/version"
+
+module Yuri
+  module Ita
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/yuri/ita_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Yuri::Ita do
+  it "has a version number" do
+    expect(Yuri::Ita::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
