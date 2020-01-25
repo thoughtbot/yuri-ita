eeb560a787d5d11a7792c7787a5815b9535c82ec
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2020-01-25 09:50:03 -0500

    Add token length to stream position if known
    This might help in debugging maybe?

--- spec/support/tokens.rb
+++ spec/support/tokens.rb
@@ -4,7 +4,8 @@ module Tokens
   def tokens(*tokens)
     tokens.map.with_index do |token, i|
       type, value = token
-      pos = RLTK::StreamPosition.new(i, 1, i, 10, nil)
+      length = (value || "").length
+      pos = RLTK::StreamPosition.new(i, 1, i, length, nil)
       RLTK::Token.new(type, value, pos)
     end
   end
