eeb5d3a3b44053e0a6ed56fa7ae28c34981bdfef
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2020-02-24 16:25:51 -0500

    Extract SearchAssembler class
    This class handles the combination of the search clauses, joining them
    together with a Merge. The merge OR's the search clauses together.

--- lib/yuriita/assembler.rb
+++ lib/yuriita/assembler.rb
@@ -1,48 +1,39 @@
-require "yuriita/clauses/merge"
-require "yuriita/or_combination"
+require "yuriita/search_assembler"
 
 module Yuriita
   class Assembler
-    def initialize(definition)
+    def initialize(definition, search_assembler: SearchAssembler)
       @definition = definition
+      @search_assembler = search_assembler
     end
 
     def build(query)
-      expression_clauses(query.expressions) +
-        scope_clauses(query.keywords, query.scopes)
+      filter_clauses(query.expressions) + keyword_search_clauses(query)
     end
 
     private
 
-    attr_reader :definition
+    attr_reader :definition, :search_assembler
 
-    def expression_clauses(expressions)
+    def filter_clauses(expressions)
       filters.flat_map do |filter|
         filter.apply(expressions)
       end
     end
 
-    def scope_clauses(keywords, scopes)
-      search_clauses = searches.flat_map do |search|
-        search.apply(keywords, scopes)
-      end
-      if search_clauses.any?
-        [
-          Clauses::Merge.new(
-            clauses: search_clauses,
-            combination: OrCombination,
-          )
-        ]
-      else
-        []
-      end
+    def keyword_search_clauses(query)
+      search_assembler.new(
+        keyword_searches: keyword_searches,
+        keywords: query.keywords,
+        scopes: query.scopes,
+      ).assemble
     end
 
     def filters
       definition.filters
     end
 
-    def searches
+    def keyword_searches
       definition.searches
     end
   end
