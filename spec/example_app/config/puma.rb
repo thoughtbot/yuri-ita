eeb5cf1126f43f778d7008d734991f7e15a2b125
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2020-03-10 18:45:28 -0400

    Update puma config rackup path

--- spec/example_app/config/puma.rb
+++ spec/example_app/config/puma.rb
@@ -3,10 +3,8 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 port        ENV.fetch("PORT") { 3000 }
-
 environment ENV.fetch("RAILS_ENV") { "development" }
-
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+rackup      File.expand_path("../config.ru", __dir__)
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
