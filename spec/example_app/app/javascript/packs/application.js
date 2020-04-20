30711de3225b894f6e0ec5569a6f1e22922ec843
Eebs Kobeissi <ebrahim.kobeissi@gmail.com>
2020-04-20 14:23:19 -0400

    Get webpack working in dev

--- spec/example_app/app/javascript/packs/application.js
+++ spec/example_app/app/javascript/packs/application.js
@@ -5,7 +5,6 @@
 
 require("@rails/ujs").start()
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
