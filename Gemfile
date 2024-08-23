dbcf9f5c3c99e9806262f8205efea1d408d4278d
louis-antonopoulos <louis@thoughtbot.com>
2024-08-23 09:53:08 -0400

    Replace webpacker with jsbundling rails (#326)
    **This PR:**
    - Replaces webpacker with jsbundling-rails since it is not supported by Rails 7
    - Updates the node version to the [latest](https://nodejs.org/en/about/previous-releases#nodejs-releases), `22.6.0`
    - Updates the Postgres image for CI
    
    **Guide:** [Upgrading to Rails 7: Moving away from Webpacker ](https://thoughtbot.com/blog/upgrading-to-rails7-moving-away-from-webpacker)
    
    Co-authored-by: Sally Hall <sally@thoughtbot.com>

--- Gemfile
+++ Gemfile
@@ -11,12 +11,12 @@ gem "bootsnap", ">= 1.4.4", require: false
 gem "bourbon"
 gem "commonmarker", "< 1.0"
 gem "github-markup", require: "github/markup"
+gem "jsbundling-rails"
 gem "pg", ">= 0.18", "< 2.0"
 gem "puma", "~> 6.0"
 gem "sass-rails", ">= 6"
 gem "sentry-raven"
 gem "themoviedb-api"
-gem "webpacker", '~> 5'
 
 group :test, :development do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
---
+++ spec/example_app/config/webpack/webpack.config.js
@@ -0,0 +1,24 @@
+const mode = process.env.NODE_ENV === "development" ? "development" : "production";
+const path    = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+  mode,
+  entry: {
+    application: "./spec/example_app/app/javascript/application.js"
+  },
+  optimization: {
+    moduleIds: "deterministic",
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    chunkFormat: "module",
+    path: path.resolve(__dirname, "..", "..", "app/assets/builds"),
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
+}
